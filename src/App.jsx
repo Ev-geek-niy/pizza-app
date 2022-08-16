aadf8f83f1d3ac0ff2553f107073d37032c92ca3
Ev-geek-niy <ev_geek_niy@vk.com>
2022-08-16 13:19:27 +0500

    Исправление перед работой
    Поправлены стили, элементы верстки вынесены в отдельные компоненты, заменен шрифт

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./scss/app.scss";
+
+import Header from "./components/header";
+import Categories from "./components/categories";
+import Sort from "./components/sort";
+import PizzaBlock from "./components/pizza-block";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title={"Пицца-бургер"} price={300} />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
